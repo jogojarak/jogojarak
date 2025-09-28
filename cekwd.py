602f5cd14cac09a0568ee0b00c79263962f2f0e7
jogojarak <danunawi02@gmail.com>
2025-09-28 21:58:45 +0700

    Update cekwd.py

--- cekwd.py
+++ cekwd.py
@@ -145,6 +145,7 @@ def cek_saldo_dan_status(playwright, situs, userid, bataswd=""):
             try:
                 batas_saldo = float(bataswd)
                 kelebihan = saldo_value - batas_saldo
+                print(f"[DEBUG] saldo={saldo_value}, batas={batas_saldo}, kelebihan={kelebihan}")
                 if kelebihan >= 50000:
                     jumlah_wd = int(kelebihan // 1000 * 1000)
                     if jumlah_wd >= 50000:
@@ -162,6 +163,28 @@ def cek_saldo_dan_status(playwright, situs, userid, bataswd=""):
                             )
             except ValueError:
                 print("Format angka di bataswd.txt tidak valid.")
+
+        # AUTO WD ALL SALDO
+        if baca_setting("AUTO_WD_ALL") == "on":
+            try:
+                wd_all_min = float(baca_setting("WD_ALL_MIN", "1000000"))  # default 200000
+                if saldo_value >= wd_all_min:
+                    jumlah_wd = int(saldo_value // 1000 * 1000)  # semua saldo kelipatan 1000
+                    if jumlah_wd >= 50000:  # tetap cek batas minimal
+                        berhasil_wd = lakukan_wd(page, situs, str(jumlah_wd))
+                        if berhasil_wd:
+                            time.sleep(5)
+                            status, nominal, tanggal = cek_status_wd(page, situs)
+                            kirim_telegram(
+                                f"<b>[AUTO-WD-ALL]</b>\n"
+                                f"👤 {userid}\n"
+                                f"💰 Rp {jumlah_wd:,.0f}\n"
+                                f"📆 {tanggal}\n"
+                                f"✅ Status: <b>{status}</b>\n"
+                                f"⌚ {wib}"
+                            )
+            except Exception as e:
+                print("Error AUTO_WD_ALL:", e)
 
         context.close()
         browser.close()
@@ -184,7 +207,7 @@ def scrape_nomor_terbaru(playwright, situs, userid):
         pass
 
     with page.expect_popup() as page1_info:
-        page.get_by_role("heading", name="HOKI DRAW").click()
+        page.get_by_role("heading", name="SYDNEY LOTTO").click()
     page1 = page1_info.value
 
     page1.get_by_role("textbox", name="-14 digit atau kombinasi huruf").fill(userid)
@@ -196,7 +219,7 @@ def scrape_nomor_terbaru(playwright, situs, userid):
     page1.get_by_role("link", name="NOMOR HISTORY NOMOR").click()
     time.sleep(2)
     page1.locator("#marketSelect").select_option("HKDW")
-    time.sleep(2)
+    time.sleep(3)
 
     page1.locator("#historyTable tbody tr").first.wait_for(timeout=10000)
     nomor = page1.locator("#historyTable tbody tr td").nth(3).inner_text().strip()
